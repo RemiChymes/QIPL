8bde2f24a30922b5363b7ba7826d7ed027942caf
RemiChymes <omikeremi@gmail.com>
2024-12-10 23:20:08 -0600

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,17 +1,19 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD2bUiZyJe2rxOgZEZWiVHUZjFsBDnafHU",
-  authDomain: "qi-platform-4b3a4.firebaseapp.com",
-  projectId: "qi-platform-4b3a4",
-  storageBucket: "qi-platform-4b3a4.appspot.com",
-  messagingSenderId: "491262763397",
-  appId: "1:491262763397:web:6b1454680b138299b2e4ec",
-  measurementId: "G-VYY94E3ZY5"
+  apiKey: "AIzaSyBLGRxAAddU0Rk_2OqFF3djsgHCRsKVKkU",
+  authDomain: "qipl-30934.firebaseapp.com",
+  projectId: "qipl-30934",
+  storageBucket: "qipl-30934.firebasestorage.app",
+  messagingSenderId: "995054408711",
+  appId: "1:995054408711:web:c93d18d60f772b93cfc6a2",
+  measurementId: "G-JRMBZMVWYL"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+const analytics = getAnalytics(app);
 
 export default app;
